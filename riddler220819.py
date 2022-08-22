9e2dac82d43dfcfc2241ac96b849d68a784e3912
Daniel Borup <48920868+borupdaniel@users.noreply.github.com>
2022-08-22 13:38:43 -0500

    Add plotting

--- riddler220819.py
+++ riddler220819.py
@@ -1,4 +1,7 @@
-def stateProb(N,nf,maxN):
+import numpy as np
+from matplotlib import pyplot as plt
+
+def stateProb(N,nf,maxN,loud=False):
     if N==maxN and nf==maxN:
         return 1.0
     if N>maxN or nf>N:
@@ -13,10 +16,11 @@ def stateProb(N,nf,maxN):
     b1 = p1 * s1
     b2 = p2 * s2
     b3 = p3 * s3
-    print(f'Probability of state N={N},nf={nf} is {100*(b1+b2+b3):.2f}%')
-    print(f'{100*b1:.2f}% from picking 2 full pills')
-    print(f'{100*b2:.2f}% from picking 3 half pills')
-    print(f'{100*b3:.2f}% from picking 1 of each')
+    if(loud):
+        print(f'Probability of state N={N},nf={nf} is {100*(b1+b2+b3):.2f}%')
+        print(f'{100*b1:.2f}% from picking 2 full pills')
+        print(f'{100*b2:.2f}% from picking 3 half pills')
+        print(f'{100*b3:.2f}% from picking 1 of each')
     return b1 + b2 + b3
 
 def pf(N,nf):
@@ -37,4 +41,10 @@ def phh(N,nf):
 def ps(N,nf):
     return 1.0 - (pf(N,nf) + phh(N,nf)) 
 
-print(f'Result = {100 * stateProb(0, 0, 15):.2f}%')
+numPills = np.arange(3,33,3,dtype=np.int32)
+rightFinalDose = 1.0 - np.array([stateProb(3,0,n) for n in numPills])
+
+plt.plot(numPills/1.5, 100 * rightFinalDose)
+plt.xlabel("Number of Days")
+plt.ylabel("\\% Chance of correct final dose")
+plt.show()
